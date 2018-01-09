17f721fa0d2841f6cdb2f849160f21e0124d487f
Reyhan Azhari <reyhan_azhari@binusian.org>
2018-01-09 08:06:39 +0700

    Preparation

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : BaseManager<GameManager>
+{
+    private TouchScreenKeyboard _keyboard;
+    private string _typedText;
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+#if UNITY_ANDROID
+        _keyboard = TouchScreenKeyboard.Open(_typedText, TouchScreenKeyboardType.Default);
+#endif
+    }
+}
---
+++ Assets/Scripts/UI/UIPop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPop : MonoBehaviour
+{
+    public Text UIText;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Execute(string text)
+    {
+        UIText.text = text;
+        _animator.SetTrigger("execute");
+    }
+}
